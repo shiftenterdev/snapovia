9abf77adcb9caa0e859b635b7ceb54104d037513
Iftakharul Alam <bappa2du@gmail.com>
2020-08-18 01:56:03 +0600

    frontend added

---
+++ resources/views/front/layouts/default.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Butikshop</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/images/favicon.ico')}}">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+
+    <!-- All css files are included here. -->
+    @include('front.partials.style')
+
+
+    <!-- Modernizr JS -->
+    <script src="{{asset('frontend/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+</head>
+
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- Body main wrapper start -->
+<div class="wrapper fixed__footer">
+    <!-- Start Header Style -->
+    @include('front.partials.header')
+    <!-- End Header Style -->
+
+    @yield('content')
+    <!-- Start Footer Area -->
+    @include('front.partials.footer')
+    <!-- End Footer Area -->
+</div>
+<!-- Body main wrapper end -->
+<!-- QUICKVIEW PRODUCT -->
+@include('front.partials.quickview')
+<!-- END QUICKVIEW PRODUCT -->
+<!-- Placed js at the end of the document so the pages load faster -->
+
+@include('front.partials.script')
+
+@yield('script')
+
+</body>
+
+</html>
---
+++ resources/views/front/partials/script.blade.php
@@ -0,0 +1,12 @@
+<!-- jquery latest version -->
+<script src="{{asset('frontend/js/vendor/jquery-1.12.0.min.js')}}"></script>
+<!-- Bootstrap framework js -->
+<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+<!-- All js plugins included in this file. -->
+<script src="{{asset('frontend/js/plugins.js')}}"></script>
+<script src="{{asset('frontend/js/slick.min.js')}}"></script>
+<script src="{{asset('frontend/js/owl.carousel.min.js')}}"></script>
+<!-- Waypoints.min.js. -->
+<script src="{{asset('frontend/js/waypoints.min.js')}}"></script>
+<!-- Main js file that contents all jQuery plugins activation. -->
+<script src="{{asset('frontend/js/main.js')}}"></script>
