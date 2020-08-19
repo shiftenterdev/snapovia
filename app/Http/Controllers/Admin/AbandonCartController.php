4e958a0838527a2c1f32df728b39a19ed914ad10
Iftakharul Alam <bappa2du@gmail.com>
2020-08-19 12:40:03 +0600

    add contribution

--- app/Http/Controllers/Admin/AbandonCartController.php
+++ app/Http/Controllers/Admin/AbandonCartController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class AbandonCartController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('admin.marketing.abandon_cart.index');
+    }
 }
---
+++ resources/views/admin/marketing/abandon_cart/index.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Abandon Cart</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Abandon Cart</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Abandon Cart</h3>
+                            </div>
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-striped table-valign-middle">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Customer</th>
+                                        <th>Customer IP</th>
+                                        <th>Subtotal</th>
+                                        <th>Total</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
