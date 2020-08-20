6d32a9dd2294ed9f35f73834e8aea2892471dd0d
mazeapi <bappa2work@gmail.com>
2020-08-20 16:05:30 +0600

    refactoring & update

--- app/Http/Controllers/Front/BlogController.php
+++ app/Http/Controllers/Front/BlogController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('front.blog.index');
+    }
+
+    public function view(Blog $blog)
+    {
+        return view('front.blog.view',compact('blog'));
+    }
 }
