95f245288c28afb43148518ffab22e6b247d63d5
Iftakharul Alam <bappa2du@gmail.com>
2020-08-22 02:07:56 +0600

    add feature and refactoring

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,39 +1,27 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login - Windmill Dashboard</title>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-        rel="stylesheet"
-    />
+    <title>Login - {{env('APP_NAME')}}</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"/>
     <link rel="stylesheet" href="{{asset('adminhtml/assets/css/tailwind.output.css')}}" />
-    <script
-        src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
-        defer
-    ></script>
-    <script src="{{asset('adminhtml/assets/js/init-alpine.js')}}"></script>
 </head>
 <body>
 <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-    <div
-        class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
-    >
+    <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div class="flex flex-col overflow-y-auto md:flex-row">
             <div class="h-32 md:h-auto md:w-1/2">
                 <img
                     aria-hidden="true"
                     class="object-cover w-full h-full dark:hidden"
                     src="{{asset('adminhtml/assets/img/login-office.jpeg')}}"
-                    alt="Office"
-                />
-                <img
-                    aria-hidden="true"
+                    alt="Admin Login"/>
+                <img aria-hidden="true"
                     class="hidden object-cover w-full h-full dark:block"
-                    src="../assets/img/login-office-dark.jpeg"
-                    alt="Office"
-                />
+                    src="{{asset('adminhtml/assets/img/login-office-dark.jpeg')}}"
+                    alt="Admin Login"/>
             </div>
             <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                 <div class="w-full">
@@ -64,15 +52,12 @@
                             @enderror
                         </label>
 
-                        <!-- You should use a button here, as the anchor is only used for the example  -->
                         <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                             Log in
                         </button>
                     </form>
 
                     <hr class="my-8" />
-
-
 
                     <p class="mt-4">
                         <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
