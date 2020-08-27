45d4a1acbfb28f02e4ee2f418125e7712a08ee86
Mazeapi <bappa2work@gmail.com>
2020-08-27 18:57:18 +0600

    Development

--- app/Repository/Eloquent/UserRepository.php
+++ app/Repository/Eloquent/UserRepository.php
@@ -4,8 +4,6 @@ namespace App\Repository\Eloquent;
 
 use App\Repository\UserRepositoryInterface;
 use App\User;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
@@ -20,20 +18,4 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
         parent::__construct($model);
     }
 
-    /**
-     * @return Collection
-     */
-    public function all(): Collection
-    {
-        return $this->model->all();
-    }
-
-    /**
-     * @param $count
-     * @return LengthAwarePaginator
-     */
-    public function paginate($count): LengthAwarePaginator
-    {
-        return $this->model->paginate($count);
-    }
 }
