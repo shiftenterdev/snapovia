7af4db35a3b1dcb6e637e5fa2cabb1f6c43f4c59
Iftakharul Alam <bappa2du@gmail.com>
2020-08-29 13:48:05 +0600

    Development

--- app/Http/Controllers/Api/Rest/CartController.php
+++ app/Http/Controllers/Api/Rest/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Rest;
 
 use App\Http\Controllers\Controller;
+use App\Http\Livewire\Admin\Product;
 use App\Models\Quote;
 use Illuminate\Http\Request;
 
@@ -17,11 +18,38 @@ class CartController extends Controller
 
     public function addItem(Request $request)
     {
-
+        $quote = Quote::where('quote_id', $request->quote_id)->firstOrFail();
+        if ($quote) {
+            $product = Product::whereSku($request->sku)->firstOrFail();
+            if ($product) {
+                $quote = Quote::items()->create([
+                    'quote_id'     => $request->quote_id,
+                    'id'           => $product->id,
+                    'sku'          => $product->sku,
+                    'name'         => $product->name,
+                    'qty'          => $request->qty,
+                    'product_type' => $product->product_type,
+                    'row_total'    => $request->qty * $product->price,
+                    'unit_price'   => $product->price,
+                ]);
+                return response()->json(['response'=>$quote],200);
+            }
+            return response()->json(['error' => 'Product not found'], 500);
+        }
+        return response()->json(['error' => 'Quote not found'], 500);
     }
 
     public function removeItem(Request $request)
     {
-
+        $quote = Quote::where('quote_id', $request->quote_id)->firstOrFail();
+        if ($quote) {
+            $product = Product::whereSku($request->sku)->firstOrFail();
+            if ($product) {
+                $quote = Quote::items()->create();
+                return response()->json(['response'=>$quote],200);
+            }
+            return response()->json(['error' => 'Product not found'], 500);
+        }
+        return response()->json(['error' => 'Quote not found'], 500);
     }
 }
