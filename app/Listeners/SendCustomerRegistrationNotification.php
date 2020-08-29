ea00317fb6e74c8608d4eb7790d49be68a919e6c
Iftakharul Alam <bappa2du@gmail.com>
2020-08-29 22:02:53 +0600

    Development

--- app/Listeners/SendCustomerRegistrationNotification.php
+++ app/Listeners/SendCustomerRegistrationNotification.php
@@ -3,19 +3,27 @@
 namespace App\Listeners;
 
 use App\Events\CustomerRegistered;
+use App\Mail\CustomerRegistrationMail;
+use App\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendCustomerRegistrationNotification
 {
     /**
+     * @var User
+     */
+    private $user;
+
+    /**
      * Create the event listener.
      *
-     * @return void
+     * @param User $user
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -26,6 +34,7 @@ class SendCustomerRegistrationNotification
      */
     public function handle(CustomerRegistered $event)
     {
-        //
+        Mail::to('iftakharul@strativ.se')
+            ->send(new CustomerRegistrationMail($this->user));
     }
 }
