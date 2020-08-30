1ad9e11ad014ba7113db68ded306696428bae5f9
Iftakharul Alam <bappa2du@gmail.com>
2020-08-30 22:34:00 +0600

    Development

--- app/Http/Controllers/Front/SearchController.php
+++ app/Http/Controllers/Front/SearchController.php
@@ -9,7 +9,12 @@ class SearchController extends Controller
 {
     public function index(Request $request)
     {
-
+        $products = \App\Models\Product::whereStatus(1)
+            ->where('name','LIKE','%'.$request->search.'%')
+            ->select(['name', 'id', 'price', 'sku'])
+            ->paginate(18);
+        $categories = [];
+        return view('front.search.index', compact('products', 'categories'));
     }
 
     public function view(Request $request)
