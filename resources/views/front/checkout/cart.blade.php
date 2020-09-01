b0b2808daddc94e65d3896be05e806dabd7ffa11
Iftakharul Alam <bappa2du@gmail.com>
2020-09-01 23:25:35 +0600

    Development

--- resources/views/front/checkout/cart.blade.php
+++ resources/views/front/checkout/cart.blade.php
@@ -1,25 +1,8 @@
 @extends('front.layouts.default')
 @section('title','Shopping Cart | ')
 @section('content')
-    <nav class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
 
-                    <!-- Breadcrumb -->
-                    <ol class="breadcrumb mb-0 font-size-xs text-gray-400">
-                        <li class="breadcrumb-item">
-                            <a class="text-gray-400" href="{{route('welcome')}}">{{__('Home')}}</a>
-                        </li>
-                        <li class="breadcrumb-item active">
-                            {{__('Shopping Cart')}}
-                        </li>
-                    </ol>
-
-                </div>
-            </div>
-        </div>
-    </nav>
+    <x-front.page.breadcrumb title="Shopping Cart"/>
 
     <livewire:front.checkout-cart/>
 
