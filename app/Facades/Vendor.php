94becb5fd452453168b5afe8f5ab35f2f5f26fbe
Iftakharul Alam <bappa2du@gmail.com>
2020-09-04 16:35:09 +0600

    Development

--- app/Facades/Vendor.php
+++ app/Facades/Vendor.php
@@ -6,6 +6,13 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Vendor
+ * @package App\Facades
+ * @method static bool attempt($credentials)
+ * @method static bool check()
+ * @method static \App\Models\Vendor user()
+ */
 class Vendor extends Facade
 {
     public static function getFacadeAccessor()
