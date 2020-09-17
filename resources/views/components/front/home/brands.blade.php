8d088c7ad77136357f806dcecf0bd0c12ea28c35
Iftakharul Alam <bappa2du@gmail.com>
2020-09-17 23:31:10 +0600

    Development

--- resources/views/components/front/home/brands.blade.php
+++ resources/views/components/front/home/brands.blade.php
@@ -1,178 +1,45 @@
-<section class="py-12 bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
+<div class="row">
+    <div class="col-4 col-sm-3 col-md text-center">
 
-                <!-- Heading -->
-                <h2 class="mb-3">@snapovia</h2>
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/mango.svg" alt="..." class="img-fluid mb-7 mb-md-0">
 
-                <!-- Subheading -->
-                <p class="mb-10 font-size-lg text-gray-500">
-                    {{__("Appear, dry there darkness they're seas, dry waters.")}}
-                </p>
-
-            </div>
-        </div>
-        <div class="row mx-n1 mb-10">
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card mb-2">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-16.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card mb-2">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-17.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card mb-2">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-18.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card mb-2">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-19.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-20.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-            <div class="col-6 col-sm-4 col-md px-1">
-
-                <!-- Card -->
-                <div class="card">
-
-                    <!-- Image -->
-                    <img src="frontend/assets/img/products/product-21.jpg" alt="..." class="card-img">
-
-                    <!-- Overlay -->
-                    <a class="card-img-overlay card-img-overlay-hover align-items-center bg-dark-40"
-                       href="https://instagram.com/myozeshop" target="_blank">
-                        <p class="my-0 font-size-xxs text-center text-white">
-                            <i class="fe fe-heart mr-2"></i> 248 <i class="fe fe-message-square mr-2 ml-3"></i> 7
-                        </p>
-                    </a>
-
-                </div>
-
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/mango.svg" alt="..." class="img-fluid mb-7 mb-md-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/zara.svg" alt="..." class="img-fluid mb-7 mb-md-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/reebok.svg" alt="..." class="img-fluid mb-7 mb-md-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/asos.svg" alt="..." class="img-fluid mb-7 mb-md-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/stradivarius.svg" alt="..."
-                     class="img-fluid mb-6 mb-sm-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/adidas.svg" alt="..." class="img-fluid mb-6 mb-sm-0">
-
-            </div>
-            <div class="col-4 col-sm-3 col-md text-center">
-
-                <!-- Brand -->
-                <img src="frontend/assets/img/brands/gray-350/bershka.svg" alt="..." class="img-fluid">
-
-            </div>
-        </div>
     </div>
-</section>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/zara.svg" alt="..." class="img-fluid mb-7 mb-md-0">
+
+    </div>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/reebok.svg" alt="..." class="img-fluid mb-7 mb-md-0">
+
+    </div>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/asos.svg" alt="..." class="img-fluid mb-7 mb-md-0">
+
+    </div>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/stradivarius.svg" alt="..."
+             class="img-fluid mb-6 mb-sm-0">
+
+    </div>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/adidas.svg" alt="..." class="img-fluid mb-6 mb-sm-0">
+
+    </div>
+    <div class="col-4 col-sm-3 col-md text-center">
+
+        <!-- Brand -->
+        <img src="frontend/assets/img/brands/gray-350/bershka.svg" alt="..." class="img-fluid">
+
+    </div>
+</div>
